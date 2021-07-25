8fe141aab5348c4da593d13e257b9d8225854289
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-07-25 17:46:29 -0300

    added products to main page

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
         <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/rolica1.png'; ?>"  />
         <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/doga1.png'; ?>"  />
     </div>
+
+    <div class="products"> 
+        <?php  
+            $args = array(
+                'post_type'      => 'product',
+                'posts_per_page' => 10,
+            );
+
+            $loop = new WP_Query( $args );
+
+            while ( $loop->have_posts() ) : $loop->the_post();
+                global $product;
+                echo '<a href="'.get_permalink().'">' . woocommerce_get_product_thumbnail().'</br> '.get_the_title();
+                echo '<p>R$' . wc_get_product()->get_price() . '</p></a>';
+            endwhile;
+
+            wp_reset_query();
+        ?>
+    </div>
     
 </div>
 
