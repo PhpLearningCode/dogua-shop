0153ab1e158742be4e1400e924639162d393f72d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-07-25 18:42:07 -0300

    fixed pagination

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <?php  
             $args = array(
                 'post_type'      => 'product',
-                'posts_per_page' => 10,
+                'posts_per_page' => 04,
             );
 
             $loop = new WP_Query( $args );
